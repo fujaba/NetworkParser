fd5aa13d4d5e65f82cb7709d23b06c7965048603
Stefan Lindel <Stefan.Lindel@cs.uni-kassel.de>
2012-03-23 13:18:22 +0000

    refactor jsonobject and jsonArray
    git-svn-id: https://jira.se.cs.uni-kassel.de/svn/NP@7 a5318cde-a0f3-428a-b0e9-3b77cec26b32

--- NetworkParser2/src/main/java/de/uni/kassel/peermessage/interfaces/SendableEntity.java
+++ NetworkParser2/src/main/java/de/uni/kassel/peermessage/interfaces/SendableEntity.java
@@ -6,6 +6,4 @@ public interface SendableEntity {
 	 public void addPropertyChangeListener(
              String propertyName,
              PropertyChangeListener listener);
-//	public void setRemoveListener(RemoveListener removeEntity);
-//	public void setUpdateListener(UpdateListener updateEntity);
 }
