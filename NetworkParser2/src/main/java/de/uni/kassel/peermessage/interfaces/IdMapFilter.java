402e06b964fbf6a6b24ca9cf8cc3cde4b19fdfcd
Stefan Lindel <Stefan.Lindel@cs.uni-kassel.de>
2012-04-23 17:07:32 +0000

    new Version with set of primitiv value
    git-svn-id: https://jira.se.cs.uni-kassel.de/svn/NP@31 a5318cde-a0f3-428a-b0e9-3b77cec26b32

--- NetworkParser2/src/main/java/de/uni/kassel/peermessage/interfaces/IdMapFilter.java
+++ NetworkParser2/src/main/java/de/uni/kassel/peermessage/interfaces/IdMapFilter.java
@@ -5,6 +5,7 @@ import de.uni.kassel.peermessage.IdMap;
 public interface IdMapFilter {
 	public boolean isConvertable(IdMap map, Object entity, String property, Object value);
 	public final static int ALLDEEP = -1;
+	public final static int LASTDEEP = 0;
 	public final static int DEEPER = -2;
 	public boolean addObject(String id);
 	public int setDeep(int value);
