06131c0e88a10e5ac5d0e52639b2a4fd82f0a2d0
Stefan Lindel <Stefan.Lindel@cs.uni-kassel.de>
2012-04-23 12:41:37 +0000

    generzation idMapfilter
    git-svn-id: https://jira.se.cs.uni-kassel.de/svn/NP@27 a5318cde-a0f3-428a-b0e9-3b77cec26b32

--- NetworkParser2/src/main/java/de/uni/kassel/peermessage/interfaces/IdMapFilter.java
+++ NetworkParser2/src/main/java/de/uni/kassel/peermessage/interfaces/IdMapFilter.java
@@ -4,4 +4,10 @@ import de.uni.kassel.peermessage.IdMap;
 
 public interface IdMapFilter {
 	public boolean isConvertable(IdMap map, Object entity, String property, Object value);
+	public final static int ALLDEEP = -1;
+	public final static int DEEPER = -2;
+	public boolean addObject(String id);
+	public int setDeep(int value);
+	public boolean existsObject(String id);
 }
+
