136da8a34b24c646f0bdc90e63742cbca0a77d62
Stefan Lindel <Stefan.Lindel@cs.uni-kassel.de>
2012-04-25 13:29:34 +0000

    remove java.beans and add stopwords to xmlidMap
    git-svn-id: https://jira.se.cs.uni-kassel.de/svn/NP@35 a5318cde-a0f3-428a-b0e9-3b77cec26b32

--- NetworkParser2/src/main/java/de/uni/kassel/peermessage/interfaces/SendableEntity.java
+++ NetworkParser2/src/main/java/de/uni/kassel/peermessage/interfaces/SendableEntity.java
@@ -2,6 +2,7 @@ package de.uni.kassel.peermessage.interfaces;
 
 import java.beans.PropertyChangeListener;
 
+
 public interface SendableEntity {
 	 public void addPropertyChangeListener(
              String propertyName,
--- NetworkParser2/src/main/java/de/uni/kassel/peermessage/xml/Decoding.java
+++ NetworkParser2/src/main/java/de/uni/kassel/peermessage/xml/Decoding.java
@@ -1,6 +1,7 @@
 package de.uni.kassel.peermessage.xml;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import de.uni.kassel.peermessage.interfaces.XMLEntityCreator;
 
@@ -14,10 +15,15 @@ public class Decoding {
 	private int pos;
 	private ArrayList<XMLEntityCreator> stack = new ArrayList<XMLEntityCreator>();
 	private ArrayList<Object> entities = new ArrayList<Object>();
+	private HashSet<String> stopwords=new HashSet<String>();
 	private XMLIdMap parent;
 
 	public Decoding(XMLIdMap parent){
 		this.parent=parent;
+		stopwords.add("?xml");
+		stopwords.add("!--");
+		stopwords.add("!DOCTYPE");
+		stopwords.add("/");
 	}
 
 	public Object decode(String value) {
@@ -64,6 +70,9 @@ public class Decoding {
 					boolean plainvalue = false;
 					String newPrefix = "";
 					if (entityCreater == null) {
+						if(stack.size()==0){
+							return null;
+						}
 						// Not found child creater
 						entityCreater = stack.get(stack.size() - 1);
 						String[] properties = entityCreater.getProperties();
@@ -123,12 +132,17 @@ public class Decoding {
 
 	private String getEntity(int start) {
 		String tag = buffer.substring(start, pos);
-		if (tag.startsWith("?xml")) {
-		} else if (tag.charAt(0) == '/') {
-		} else {
-			return tag;
+		for(String stopword : stopwords){
+			if(tag.startsWith(stopword)){
+				return "";
+			}
 		}
-		return "";
+		return tag;
+	}
+	public void addStopWords(String... stopwords){
+		for(String stopword : stopwords){
+			this.stopwords.add(stopword);
+		}
 	}
 
 	private void convertParams(XMLEntityCreator entityCreater, Object entity,
@@ -151,5 +165,4 @@ public class Decoding {
 			}
 		}
 	}
-
 }
