c07e3d6cd10690a6cbcc5c2ba077c513bba3541d
Stefan Lindel <Stefan.Lindel@cs.uni-kassel.de>
2012-12-05 14:06:33 +0000

    new Version

--- de/uniks/jism/IdMap.java
+++ de/uniks/jism/IdMap.java
@@ -300,6 +300,16 @@ public class IdMap implements Map<String, Object> {
 	public SendableEntityCreator getCreatorClass(Object reference) {
 		return getCreatorClasses(reference.getClass().getName());
 	}
+	public SendableEntityCreator getCreatorClassName(String clazz) {
+		clazz="."+clazz;
+		for(Iterator<Entry<String, SendableEntityCreator>> i = this.creators.entrySet().iterator();i.hasNext();){
+			Entry<String, SendableEntityCreator> entry = i.next();
+			if(entry.getKey().endsWith(clazz)){
+				return entry.getValue();
+			}
+		}
+		return null;
+	}
 
 	/**
 	 * Clone object.
