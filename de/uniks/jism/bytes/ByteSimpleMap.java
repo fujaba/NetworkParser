62f7b2f9ca4ce055149b33d73c67e0c48fc5d018
Stefan Lindel <Stefan.Lindel@cs.uni-kassel.de>
2013-01-25 21:40:15 +0000

    add BitDecoder

--- de/uniks/jism/bytes/ByteSimpleMap.java
+++ de/uniks/jism/bytes/ByteSimpleMap.java
@@ -1,13 +1,13 @@
 package de.uniks.jism.bytes;
 
 import java.nio.ByteBuffer;
+import java.util.HashMap;
 
 import de.uniks.jism.AbstractIdMap;
 import de.uniks.jism.CloneFilter;
 import de.uniks.jism.IdMap;
-import de.uniks.jism.interfaces.BitEntity;
-import de.uniks.jism.interfaces.BitEntityCreator;
-import de.uniks.jism.test.model.creator.NumberFormatCreator;
+import de.uniks.jism.event.BitEntity;
+import de.uniks.jism.event.creator.BitEntityCreator;
 
 public class ByteSimpleMap extends AbstractIdMap{
 
@@ -16,11 +16,16 @@ public class ByteSimpleMap extends AbstractIdMap{
 		return null;
 	}
 	
+//	public Object decode(ByteBuffer buffer){
+//		
+//	}
+	
 	public Object decode(ByteBuffer buffer, BitEntityCreator creator){
+		HashMap<String, Object> values=new HashMap<String, Object>();
 		BitEntity[] bitProperties = creator.getBitProperties();
 		Object newInstance = creator.getSendableInstance(false);
 		for(BitEntity entity : bitProperties){
-			Object element = get(buffer, entity);
+			Object element = getEntity(buffer, entity, values);
 			if(element != null){
 				creator.setValue(newInstance, entity.getPropertyName(), element, IdMap.NEW);
 			}
@@ -28,25 +33,90 @@ public class ByteSimpleMap extends AbstractIdMap{
 		return newInstance;
 	}
 	
-	public Object get(ByteBuffer buffer, BitEntity entry)
-	{
-		//FIXME New Parser
-		Object result = null;
-		boolean finished=false;
-		do
-		{
-//			finished = current.equals(lsb);
-//			result = result << 1;
-//			ensureSize(current.bytePos+1);
-//			byte theByte = array[current.bytePos];
-////			final int i = BYTE_SIZE-currentPosition.bitPos-1;
-//			int bitvalue = (theByte >> current.bitPos)&1;
-//			result += bitvalue;
-//			
-//			current.minusIntern(step);
+	public Object getEntity(ByteBuffer buffer, BitEntity entry, HashMap<String, Object> values){
+		if(entry.isTyp(BitEntity.TYP_VALUE)){
+			return entry.getPropertyName();
+		}else if(entry.isTyp(BitEntity.TYP_REFERENCE)){
+			String propertyName = entry.getPropertyName();
+			if(values.containsKey(propertyName)){
+				return values.get(propertyName);
+			}
 		}
-		while (!finished);
-		return result;
-	}
+		
+		// Init the Values
+		int len = Integer.valueOf(""+getEntity(buffer, entry.getStartBit(), values));;
+		int posOfByte = len/8;
+		int posOfBit = len%8;
+		
+		len = Integer.valueOf(""+getEntity(buffer, entry.getLen(), values));
+		int noOfByte=len/8;
+		if(len%8>0){
+			noOfByte++;
+		}
+		
+		int number = 0;
+		int resultPos = 0;
 
+		ByteBuffer result = ByteBuffer.allocate(noOfByte);
+		byte theByte = buffer.get(posOfByte);
+		while(len>0){
+			int bitvalue = (theByte >> posOfBit)&1;
+			number += bitvalue << resultPos;
+			posOfBit++;
+			resultPos++;
+			if(posOfBit>7){
+				posOfBit = 0;
+				++posOfByte;
+				if(posOfByte<buffer.limit()){
+					theByte = buffer.get(posOfByte);
+				}
+			}
+			if(resultPos ==8){
+				result.put((byte)number);
+				resultPos = 0;
+			}
+			len--;
+		}
+		if(resultPos>0){
+			result.put((byte)number);
+		}
+		// Set the Typ
+		Object element=null;
+
+		result.flip();
+		if(entry.getTyp().equals(BitEntity.BIT_BYTE)){
+			byte[] array = result.array();
+			if(array.length==1){
+				element = new Byte(array[0]);
+			}else{
+				Byte[] item=new Byte[array.length];
+				for(int i=0;i<array.length;i++){
+					item[i] = array[i];
+				}
+				element = item;
+			}
+		}else if(entry.getTyp().equals(BitEntity.BIT_NUMBER)){
+			if(result.limit()== Byte.SIZE/ByteEntity.BITOFBYTE){
+				element = result.get();
+			}else if(result.limit()== Short.SIZE/ByteEntity.BITOFBYTE){
+				element = result.getShort();
+			} else if(result.limit()== Integer.SIZE/ByteEntity.BITOFBYTE){
+				element = result.getInt();
+			} else if(result.limit()== Long.SIZE/ByteEntity.BITOFBYTE){
+				element = result.getLong();
+			} else if(result.limit()== Float.SIZE/ByteEntity.BITOFBYTE){
+				element = result.getFloat();
+			} else if(result.limit()== Double.SIZE/ByteEntity.BITOFBYTE){
+				element = result.getDouble();
+			}else{
+				element = result.getInt();
+			}
+		}else if(entry.getTyp().equals(BitEntity.BIT_STRING)){
+			result.flip();
+			element = new String(result.array());
+			
+		}
+		values.put(entry.getPropertyName(), element);
+		return element;
+	}
 }
