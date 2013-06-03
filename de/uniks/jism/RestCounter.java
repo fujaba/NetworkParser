2e6a1ff75149ad526da07550beba5c6e55380fa6
Stefan Lindel <Stefan.Lindel@cs.uni-kassel.de>
2013-06-03 12:20:13 +0000

    fix RestCounter for subdirectory and id for objects

--- de/uniks/jism/RestCounter.java
+++ de/uniks/jism/RestCounter.java
@@ -2,17 +2,31 @@ package de.uniks.jism;
 
 public class RestCounter extends SimpleIdCounter{
 	
-	private String path;
-
 	public RestCounter(String path){
-		this.path = path;
+		if(path.endsWith("/")){
+			this.prefixId = path;
+		}else{
+			this.prefixId = path+"/";
+		}
 	}
 	
 	@Override
 	public String getId(Object obj) {
-		return path+super.getId(obj);
-	}
+		String key;
 
+		// new object generate key and add to tables
+		// <session id>.<first char><running number>
+		if (obj == null) {
+			Exception e = new Exception("NullPointer: " + obj);
+			e.printStackTrace();
+			return "";
+		}
+		String className = obj.getClass().getName();
+		key = this.prefixId + className.toLowerCase() +"/"+ this.number;
+		this.number++;
+		return key;
+	}
+	
 	@Override
 	public boolean isSimpleObject() {
 		return true;
