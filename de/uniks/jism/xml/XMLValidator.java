355b9da5f32901f09eac06671ad7bd101883489d
Stefan Lindel <Stefan.Lindel@cs.uni-kassel.de>
2013-08-07 14:28:21 +0000

    gwt module

--- de/uniks/jism/xml/XMLValidator.java
+++ de/uniks/jism/xml/XMLValidator.java
@@ -37,20 +37,21 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import org.xml.sax.SAXException;
-import com.sun.org.apache.xerces.internal.jaxp.validation.XMLSchemaFactory;
+//import com.sun.org.apache.xerces.internal.jaxp.validation.XMLSchemaFactory;
 
 public class XMLValidator {
 	public static XsdValidationLoggingErrorHandler validate(java.net.URL xsdSchema, String xmlDokument)
 			throws SAXException, IOException {
-		com.sun.org.apache.xerces.internal.jaxp.validation.XMLSchemaFactory schemaFactory = (XMLSchemaFactory) SchemaFactory
-				.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-
-//		Schema schema = schemaFactory.newSchema(new File(xsdSchema));
-		Schema schema = schemaFactory.newSchema(xsdSchema);
-		Validator validator = schema.newValidator();
-		XsdValidationLoggingErrorHandler errorHandler = new XsdValidationLoggingErrorHandler();
-		validator.setErrorHandler(errorHandler);
-		validator.validate(new StreamSource(new File(xmlDokument)));
-		return errorHandler;
+//		com.sun.org.apache.xerces.internal.jaxp.validation.XMLSchemaFactory schemaFactory = (XMLSchemaFactory) SchemaFactory
+//				.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+//
+////		Schema schema = schemaFactory.newSchema(new File(xsdSchema));
+//		Schema schema = schemaFactory.newSchema(xsdSchema);
+//		Validator validator = schema.newValidator();
+//		XsdValidationLoggingErrorHandler errorHandler = new XsdValidationLoggingErrorHandler();
+//		validator.setErrorHandler(errorHandler);
+//		validator.validate(new StreamSource(new File(xmlDokument)));
+//		return errorHandler;
+		return null;
 	}
 }
