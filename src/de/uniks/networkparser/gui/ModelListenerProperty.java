ed75bdaf814252a7a44dd9f76f9c779e3ddb60ac
Stefan Lindel <Stefan.Lindel@cs.uni-kassel.de>
2013-12-10 11:25:29 +0100

    change Licencetext

--- src/de/uniks/networkparser/gui/ModelListenerProperty.java
+++ src/de/uniks/networkparser/gui/ModelListenerProperty.java
@@ -1,11 +1,31 @@
 package de.uniks.networkparser.gui;
 
+/*
+ NetworkParser
+ Copyright (c) 2011 - 2013, Stefan Lindel
+ All rights reserved.
+ 
+ Licensed under the EUPL, Version 1.1 or – as soon they
+ will be approved by the European Commission - subsequent
+ versions of the EUPL (the "Licence");
+ You may not use this work except in compliance with the Licence.
+ You may obtain a copy of the Licence at:
+
+ http://ec.europa.eu/idabc/eupl5
+
+ Unless required by applicable law or agreed to in
+ writing, software distributed under the Licence is
+ distributed on an "AS IS" basis,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ express or implied.
+ See the Licence for the specific language governing
+ permissions and limitations under the Licence.
+*/
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.lang.reflect.Method;
 import java.util.LinkedHashSet;
-
 import javafx.beans.InvalidationListener;
 import javafx.beans.property.Property;
 import javafx.beans.property.SimpleObjectProperty;
@@ -15,9 +35,8 @@ import de.uniks.networkparser.IdMap;
 import de.uniks.networkparser.interfaces.SendableEntity;
 import de.uniks.networkparser.interfaces.SendableEntityCreator;
 
-
 public abstract class ModelListenerProperty<T> implements javafx.beans.property.Property<T>, PropertyChangeListener, ObservableValue<T>{
-    private Object item;
+	private Object item;
     private String property;
     private SendableEntityCreator creator;
     private LinkedHashSet<ChangeListener<? super T>> listeners=new LinkedHashSet<ChangeListener<? super T>>();
