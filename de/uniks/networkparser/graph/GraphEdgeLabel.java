a345409a39e337a12a24c9444c3b2dcd9de22c6f
Stefan Lindel <Stefan.Lindel@cs.uni-kassel.de>
2014-02-26 21:19:14 +0100

    add Graph Map and javascript docu

---
+++ de/uniks/networkparser/graph/GraphEdgeLabel.java
@@ -0,0 +1,35 @@
+package de.uniks.networkparser.graph;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class GraphEdgeLabel {
+	private ArrayList<GraphNode> items = new ArrayList<GraphNode>();
+	private String cardinality;
+	private String property;
+	
+	public ArrayList<GraphNode> getItems() {
+		return items;
+	}
+	public GraphEdgeLabel withItem(GraphNode item) {
+		this.items.add(item);
+		return this;
+	}
+	public String getCardinality() {
+		return cardinality;
+	}
+	public GraphEdgeLabel withCardinality(String cardinality) {
+		this.cardinality = cardinality;
+		return this;
+	}
+	public String getProperty() {
+		return property;
+	}
+	public GraphEdgeLabel withProperty(String property) {
+		this.property = property;
+		return this;
+	}
+	public Iterator<GraphNode> iterator(){
+		return items.iterator();
+	}
+}
