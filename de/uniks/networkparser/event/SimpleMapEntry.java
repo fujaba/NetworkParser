e773e02dfdbc195acb1a23f753e99ce96547d116
Stefan Lindel <Stefan.Lindel@cs.uni-kassel.de>
2014-06-23 12:01:52 +0200

    add Javadoc and Licence

--- de/uniks/networkparser/event/SimpleMapEntry.java
+++ de/uniks/networkparser/event/SimpleMapEntry.java
@@ -4,7 +4,7 @@ package de.uniks.networkparser.event;
  NetworkParser
  Copyright (c) 2011 - 2013, Stefan Lindel
  All rights reserved.
-
+ 
  Licensed under the EUPL, Version 1.1 or (as soon they
  will be approved by the European Commission) subsequent
  versions of the EUPL (the "Licence");
@@ -20,8 +20,7 @@ package de.uniks.networkparser.event;
  express or implied.
  See the Licence for the specific language governing
  permissions and limitations under the Licence.
- */
-
+*/
 import de.uniks.networkparser.AbstractKeyValueEntry;
 
 public class SimpleMapEntry<K,V> extends AbstractKeyValueEntry<K, V> {
--- de/uniks/networkparser/graph/GraphConverter.java
+++ de/uniks/networkparser/graph/GraphConverter.java
@@ -1,17 +1,36 @@
 package de.uniks.networkparser.graph;
 
+/*
+ NetworkParser
+ Copyright (c) 2011 - 2013, Stefan Lindel
+ All rights reserved.
+ 
+ Licensed under the EUPL, Version 1.1 or (as soon they
+ will be approved by the European Commission) subsequent
+ versions of the EUPL (the "Licence");
+ You may not use this work except in compliance with the Licence.
+ You may obtain a copy of the Licence at:
+
+ http://ec.europa.eu/idabc/eupl5
+
+ Unless required by applicable law or agreed to in
+ writing, software distributed under the Licence is
+ distributed on an "AS IS" basis,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ express or implied.
+ See the Licence for the specific language governing
+ permissions and limitations under the Licence.
+*/
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
-
 import de.uniks.networkparser.AbstractKeyValueEntry;
 import de.uniks.networkparser.json.JsonArray;
 import de.uniks.networkparser.json.JsonIdMap;
 import de.uniks.networkparser.json.JsonObject;
-
 
 public class GraphConverter implements Converter {
 	public static final String TYP="typ";
