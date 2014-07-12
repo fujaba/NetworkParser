7d13862c570c2c49fed0810a61eb0d4b041b28af
Stefan Lindel <slin@cs.uni-kassel.de>
2014-07-12 16:45:11 +0200

    add Licence and fix return value

--- de/uniks/networkparser/AbstractEntityList.java
+++ de/uniks/networkparser/AbstractEntityList.java
@@ -1,5 +1,25 @@
 package de.uniks.networkparser;
+/*
+NetworkParser
+Copyright (c) 2011 - 2013, Stefan Lindel
+All rights reserved.
 
+Licensed under the EUPL, Version 1.1 or (as soon they
+will be approved by the European Commission) subsequent
+versions of the EUPL (the "Licence");
+You may not use this work except in compliance with the Licence.
+You may obtain a copy of the Licence at:
+
+http://ec.europa.eu/idabc/eupl5
+
+Unless required by applicable law or agreed to in
+writing, software distributed under the Licence is
+distributed on an "AS IS" basis,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+express or implied.
+See the Licence for the specific language governing
+permissions and limitations under the Licence.
+*/
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -65,11 +85,12 @@ public abstract class AbstractEntityList<V> extends AbstractList<V> implements L
 		return this;
 	}
     
-   public AbstractList<V> with(Collection<?> values) {
+   @SuppressWarnings("unchecked")
+   public <ST extends AbstractList<V>> ST with(Collection<?> values) {
 		for(Iterator<?> i = values.iterator();i.hasNext();){
 			with( i.next() );
 		}
-		return this;
+		return (ST)this;
 	}
 	
 	public Collection<V> values() {
