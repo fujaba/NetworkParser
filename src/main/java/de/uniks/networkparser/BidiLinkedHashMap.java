e333edfc21fa4b2f9bdedce7487fd41a05d4de46
Stefan Lindel <slin@cs.uni-kassel.de>
2014-07-25 21:37:06 +0200

    start to new Version of Bidimap extract all to seperate Class
    Former-commit-id: 5e314f5a132b589e50619e4f4d3a6b3bd36e0b15

---
+++ src/main/java/de/uniks/networkparser/BidiLinkedHashMap.java
@@ -0,0 +1,86 @@
+package de.uniks.networkparser;
+/*
+NetworkParser
+Copyright (c) 2011 - 2013, Stefan Lindel
+All rights reserved.
+
+Licensed under the EUPL, Version 1.1 or (as soon they
+will be approved by the European Commission) subsequent
+versions of the EUPL (the "Licence");
+You may not use this work except in compliance with the Licence.
+You may obtain a copy of the Licence at:
+
+http://ec.europa.eu/idabc/eupl5
+
+Unless required by applicable law or agreed to in
+writing, software distributed under the Licence is
+distributed on an "AS IS" basis,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+express or implied.
+See the Licence for the specific language governing
+permissions and limitations under the Licence.
+*/
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+import de.uniks.networkparser.interfaces.BidiMap;
+
+public class BidiLinkedHashMap implements BidiMap<String, Object>{
+	protected LinkedHashMap<String, Object> keyValue = new LinkedHashMap<String, Object>();
+	protected LinkedHashMap<Object, String> valueKey = new LinkedHashMap<Object, String>();
+
+	@Override
+	public int size() {
+		return keyValue.size();
+	}
+
+	@Override
+	public void clear() {
+		keyValue.clear();
+		valueKey.clear();
+	}
+
+	@Override
+	public Collection<Object> values() {
+		return keyValue.values();
+	}
+
+	@Override
+	public Set<String> keySet() {
+		return keyValue.keySet();
+	}
+
+	@Override
+	public boolean containsKey(String key) {
+		return keyValue.containsKey(key);
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		return valueKey.containsKey(value);
+	}
+
+	@Override
+	public BidiLinkedHashMap without(String key, Object value) {
+		this.keyValue.remove(key);
+		this.valueKey.remove(value);
+		return this;
+	}
+	
+	public BidiLinkedHashMap with(String key, Object value) {
+		this.keyValue.put(key, value);
+		this.valueKey.put(value, key);
+		return this;
+	}
+
+	@Override
+	public Object getValue(String key) {
+		return keyValue.get(key);
+	}
+
+	@Override
+	public String getKey(Object key) {
+		return valueKey.get(key);
+	}
+}
