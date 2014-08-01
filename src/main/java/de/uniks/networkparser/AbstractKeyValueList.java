45659678850579f67d518e313ae2edf2b63856f0
Albert Zuendorf <zuendorf@cs.uni-kassel.de>
2014-08-01 15:36:57 +0200

    allow for each loop for keys
    Former-commit-id: 303122cc0ae96cbf7bcf5526fe3fb641087e1ffd

--- src/main/java/de/uniks/networkparser/AbstractKeyValueList.java
+++ src/main/java/de/uniks/networkparser/AbstractKeyValueList.java
@@ -25,6 +25,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
 import java.util.Set;
@@ -213,6 +214,11 @@ public abstract class AbstractKeyValueList<K, V> extends AbstractList<K> impleme
 	@Override
 	public HashSet<K> keySet() {
 		return new HashSet<K>(this.keys);
+	}
+	
+	public List<K> keyList()
+	{
+	   return this.keys;
 	}
 	
 	/**
