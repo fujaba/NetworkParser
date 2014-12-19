8ee099cc7460d0f42db8dede72799c2017a0d2d5
Stefan Lindel <slin@cs.uni-kassel.de>
2014-12-19 16:57:13 +0100

    add Random method
    Former-commit-id: 4633bed3e81690b77cb92dcb00dd7f811462d7d3

--- NetworkParserFX/src/main/java/de/uniks/networkparser/gui/controller/PointPaneController.java
+++ NetworkParserFX/src/main/java/de/uniks/networkparser/gui/controller/PointPaneController.java
@@ -3,6 +3,7 @@ package de.uniks.networkparser.gui.controller;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
+import java.util.Random;
 
 import javafx.event.EventHandler;
 import javafx.scene.Node;
@@ -101,6 +102,29 @@ public class PointPaneController extends AbstractModelController implements Prop
 			}
 			this.setValue(val);
 		}
+	}
+	
+	/**
+	 * Returns a pseudo-random number between min and max, inclusive.
+	 * The difference between min and max can be at most
+	 * <code>Integer.MAX_VALUE - 1</code>.
+	 *
+	 * @param min Minimum value
+	 * @param max Maximum value.  Must be greater than min.
+	 * @return Integer between min and max, inclusive.
+	 * @see java.util.Random#nextInt(int)
+	 */
+	public int randInt(int min, int max) {
+
+	    // NOTE: Usually this should be a field rather than a method
+	    // variable so that it is not re-seeded every call.
+	    Random rand = new Random();
+
+	    // nextInt is normally exclusive of the top value,
+	    // so add 1 to make it inclusive
+	    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+	    return randomNum;
 	}
 
 	@Override
