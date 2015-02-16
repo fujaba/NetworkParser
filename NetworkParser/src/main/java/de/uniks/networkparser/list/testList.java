6eb0f564f9c518138d550b3f4cf1ba3b41e485b8
Albert Zuendorf <zuendorf@cs.uni-kassel.de>
2015-02-16 16:59:29 +0100

    first simple functionality test
    Former-commit-id: 6e34b697966ce540ae53f7ad495093e8a5d4ca70

--- NetworkParser/src/main/java/de/uniks/networkparser/list/testList.java
+++ NetworkParser/src/main/java/de/uniks/networkparser/list/testList.java
@@ -1,6 +1,7 @@
 package de.uniks.networkparser.list;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class testList {
 
@@ -17,5 +18,18 @@ public class testList {
 		map.add("Stefan", 42);
 		
 		System.out.println(map);
+	}
+	
+	@Test
+	public void simpleListFunctionality()
+	{
+		// create a set of some 42 names
+		SimpleList<Integer> simpleList = new SimpleList<Integer>();
+		
+		Integer int_01 = new Integer(1);
+		simpleList.add(int_01);
+		
+		assertTrue("List should contain added element", simpleList.contains(int_01));
+		
 	}
 }
