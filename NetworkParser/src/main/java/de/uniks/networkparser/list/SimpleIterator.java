87d76664cd17827100a3bfd429b72042b3aeb8e4
Albert Zuendorf <zuendorf@cs.uni-kassel.de>
2015-02-17 17:28:58 +0100

    SimpleListIterator coverage 89%

--- NetworkParser/src/main/java/de/uniks/networkparser/list/SimpleIterator.java
+++ NetworkParser/src/main/java/de/uniks/networkparser/list/SimpleIterator.java
@@ -14,83 +14,81 @@ public class SimpleIterator<E> implements ListIterator<E> {
 	private AbstractList<E> list;
 
 	public SimpleIterator(AbstractList<E> list) {
-        this.list = list;
+		this.list = list;
 	}
-	
+
 	public SimpleIterator(AbstractList<E> list, int index) {
-        this.cursor = index;
-        this.list = list;
+		this.cursor = index;
+		this.list = list;
 	}
 
 	public boolean hasPrevious() {
-        return cursor != 0;
-    }
+		return cursor != 0;
+	}
 
-    public int nextIndex() {
-        return cursor;
-    }
+	public int nextIndex() {
+		return cursor;
+	}
 
-    public int previousIndex() {
-        return cursor - 1;
-    }
+	public int previousIndex() {
+		return cursor - 1;
+	}
 
-    public E previous() {
-        int i = cursor - 1;
-        if (i < 0)
-            throw new NoSuchElementException();
-        
-        cursor = i;
-        return list.get(lastRet = i);
-    }
+	public E previous() {
+		int i = cursor - 1;
+		if (i < 0)
+			throw new NoSuchElementException();
 
-    public void set(E e) {
-        if (lastRet < 0)
-            throw new IllegalStateException();
-        try {
-            list.set(lastRet, e);
-        } catch (IndexOutOfBoundsException ex) {
-            throw new ConcurrentModificationException();
-        }
-    }
+		cursor = i;
+		return list.get(lastRet = i);
+	}
 
-    public void add(E e) {
-        try {
-            int i = cursor;
-            list.add(i, e);
-            cursor = i + 1;
-            lastRet = -1;
-        } catch (IndexOutOfBoundsException ex) {
-            throw new ConcurrentModificationException();
-        }
-    }
-   
+	public void set(E e) {
+		if (lastRet < 0)
+			throw new IllegalStateException();
+		try {
+			list.set(lastRet, e);
+		} catch (IndexOutOfBoundsException ex) {
+			throw new ConcurrentModificationException();
+		}
+	}
+
+	public void add(E e) {
+		try {
+			int i = cursor;
+			list.add(i, e);
+			cursor = i + 1;
+			lastRet = -1;
+		} catch (IndexOutOfBoundsException ex) {
+			throw new ConcurrentModificationException();
+		}
+	}
+
 	@Override
 	public boolean hasNext() {
 		return cursor<list.size();
 	}
-		
+
 	@Override
 	public E next() {
-		 int i = cursor;
-        if (i >= list.size())
-            throw new NoSuchElementException();
-        if (i >= list.size())
-            throw new ConcurrentModificationException();
-        cursor = i + 1;
-        return list.get(lastRet = i);
+		int i = cursor;
+		if (i >= list.size())
+			throw new ConcurrentModificationException();
+		cursor = i + 1;
+		return list.get(lastRet = i);
 	}
 
 	@Override
 	public void remove() {
 		if (lastRet < 0)
-            throw new IllegalStateException();
-        try {
-            list.remove(lastRet);
-            cursor = lastRet;
-            lastRet = -1;
-        } catch (IndexOutOfBoundsException ex) {
-            throw new ConcurrentModificationException();
-        }
+			throw new IllegalStateException();
+		try {
+			list.remove(lastRet);
+			cursor = lastRet;
+			lastRet = -1;
+		} catch (IndexOutOfBoundsException ex) {
+			throw new ConcurrentModificationException();
+		}
 	}
 }
 
