909a8c2b048e63b2a83aa0697e4fe4fa548def2a
Stefan Lindel <slin@cs.uni-kassel.de>
2015-02-21 17:59:42 +0100

    add a failure test
    Former-commit-id: f2e87eecc72910e1bd765c87359259df55456646

---
+++ NetworkParser/src/test/java/de/uniks/networkparser/test/ArrayListTest.java
@@ -0,0 +1,42 @@
+package de.uniks.networkparser.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.uniks.networkparser.list.SimpleList;
+
+public class ArrayListTest {
+
+	@Test
+	public void testReadd() {
+		SimpleList<Integer> list=new SimpleList<Integer>();
+		
+		Assert.assertFalse(list.contains(3));
+		Assert.assertFalse(list.contains(4));
+		Assert.assertFalse(list.contains(5));
+		
+		for(int i=1;i<=500;i++) {
+			list.add(i);
+		}
+		Assert.assertEquals(500, list.size());
+		
+		Assert.assertTrue(list.contains(3));
+		Assert.assertTrue(list.contains(4));
+		Assert.assertTrue(list.contains(5));
+		
+		
+		list.remove(4);
+		list.remove(3);
+		list.remove(2);
+		
+		
+		Assert.assertFalse(list.contains(3));
+		Assert.assertFalse(list.contains(4));
+		Assert.assertFalse(list.contains(5));
+		
+		list.add(3);
+		
+		Assert.assertTrue(list.contains(3));
+		
+	}
+}
