1687b6d410b347090d7d211a29bad174e31ca110
Stefan Lindel <slin@cs.uni-kassel.de>
2015-02-23 15:00:02 +0100

    remove old Lists
    Former-commit-id: b99231e55c5bd08c59485c7bb37b69b857ff4576

--- NetworkParser/src/main/java/de/uniks/networkparser/listold/not/KeyValueMap.java
+++
@@ -1,8 +0,0 @@
-package de.uniks.networkparser.listold.not;
-
-import java.util.Map;
-
-public interface KeyValueMap<K, V> extends Map<K, V>, Iterable<K> {
-	public boolean addEntity(K key, V value);
-	public int addValue(int pos, V value);
-}
