cc3e387f30b714aee92f39d5fe21ed4069913f72
Stefan Lindel <slin@cs.uni-kassel.de>
2015-03-05 14:58:21 +0100

    add field to genericCreator
    Former-commit-id: 7e55e4f65241d2d748ced79c2cb94c24d2ba7f57

---
+++ NetworkParserFX/src/test/java/de/uniks/networkparser/gui/test/TextItemGUI.java
@@ -0,0 +1,44 @@
+package de.uniks.networkparser.gui.test;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import de.uniks.networkparser.gui.controller.ModelListenerFactory;
+import de.uniks.networkparser.gui.dialog.DialogBox;
+
+public class TextItemGUI extends Application{
+		private String name="bla";
+		@Override
+		public void start(Stage stage) throws Exception {
+
+			AnchorPane root=new AnchorPane();
+			Scene scene = new Scene(root, 600, 400);
+			TextField label=new TextField();
+			ModelListenerFactory.create(label, this, "name");
+			
+			HBox box = new HBox();
+			Button button=new Button("display"); 
+			button.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent arg0) {
+					DialogBox.showInfo("Name", "The name is: "+name);
+				}
+			});
+			box.getChildren().addAll( label, button);
+			root.getChildren().add(box);
+			stage.setTitle("SimpleGUI");
+			stage.setScene(scene);
+			stage.show();
+		}
+		public static void main(String[] args) {
+			launch(args);
+		}
+		
+
+	}
