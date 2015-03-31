3b2d92a5468347486b19fb5675f4a041559de569
Stefan Lindel <slin@cs.uni-kassel.de>
2015-03-31 16:46:20 +0200

    fix TableComponent

--- NetworkParserFX/src/main/java/de/uniks/networkparser/gui/table/TableComponent.java
+++ NetworkParserFX/src/main/java/de/uniks/networkparser/gui/table/TableComponent.java
@@ -517,7 +517,7 @@ public class TableComponent extends BorderPane implements PropertyChangeListener
                 }
             }
             if((columns.size() + subColumns.size())<1) {
-            	System.out.println("FIXME DONT FIND COLUMN: " + event.getPropertyName());
+//            	System.out.println("FIXME DONT FIND COLUMN: " + event.getPropertyName());
             }else {
             	for(TableColumnFX column  : columns ) {
                 	Object item = event.getSource();
