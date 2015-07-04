36604ea0348f9df48051a20dde6417268685401e
Stefan Lindel <slin@cs.uni-kassel.de>
2015-07-04 09:24:13 +0200

    add some text to readme.md

---
+++ src/test/java/de/uniks/networkparser/test/model/House.java
@@ -0,0 +1,57 @@
+package de.uniks.networkparser.test.model;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+import de.uniks.networkparser.interfaces.SendableEntity;
+import de.uniks.networkparser.interfaces.SendableEntityCreator;
+
+public class House implements SendableEntityCreator, SendableEntity{
+	public static final String PROPERTY_NAME = "name";
+	public static final String PROPERTY_FLOOR = "floor";
+	protected PropertyChangeSupport listeners = new PropertyChangeSupport(this);
+	private String name;
+	private int floor;
+	public Object getValue(Object entity, String attribute) {
+		if(PROPERTY_NAME.equals(attribute)){
+			return getName();
+		}
+		if(PROPERTY_FLOOR.equals(attribute)){
+			return getFloor();
+		}
+		return null;
+	}
+	public boolean setValue(Object entity, String attribute, Object value, String type) {
+		if(PROPERTY_NAME.equals(attribute)){
+			((House)entity).setName(""+value);
+			return true;
+		}
+		if(PROPERTY_FLOOR.equals(attribute)){
+			((House)entity).setFloor((Integer)value);
+			return true;
+		}
+		return false;
+	}
+	public void setFloor(int value) {
+		if(value!=this.floor){
+			Object oldValue = this.floor;
+			this.floor = value;
+			getPropertyChangeSupport().firePropertyChange(PROPERTY_FLOOR, oldValue, value);
+		}	
+	}
+	public void setName(String value) {
+		if(value!=this.name){
+			Object oldValue = this.name;
+			this.name = value;
+			getPropertyChangeSupport().firePropertyChange(PROPERTY_NAME, oldValue, value);
+		}	
+	}
+	public String[] getProperties() {return new String[]{PROPERTY_NAME, PROPERTY_FLOOR};}
+	public Object getSendableInstance(boolean prototyp) {return new House();}
+	public int getFloor() {return floor;}
+	public String getName() {return name;}
+	public PropertyChangeSupport getPropertyChangeSupport(){return listeners;}
+	public boolean addPropertyChangeListener(String propertyName, PropertyChangeListener listener) {getPropertyChangeSupport().addPropertyChangeListener(propertyName, listener);return true;}
+	public boolean addPropertyChangeListener(PropertyChangeListener listener) {getPropertyChangeSupport().addPropertyChangeListener(listener);return true;}
+	public boolean removePropertyChangeListener(PropertyChangeListener listener) {getPropertyChangeSupport().removePropertyChangeListener(listener);return true;}
+}
