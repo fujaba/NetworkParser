0e19f6b04aa3c1525b6b5917ee3d1c041a7d6563
Albert Zuendorf <zuendorf@cs.uni-kassel.de>
2015-07-20 16:21:03 +0200

    some larger numbers
    Former-commit-id: 9c74f3a4e24ba8d9a513b1378cc11091be4b5fbe

--- src/test/java/de/uniks/networkparser/test/ArrayListTest.java
+++ src/test/java/de/uniks/networkparser/test/ArrayListTest.java
@@ -7,88 +7,87 @@ import de.uniks.networkparser.list.SimpleList;
 
 public class ArrayListTest {
 
-	@Test
-	public void testReadd() {
-		SimpleList<Integer> list=new SimpleList<Integer>();
-		
-		Assert.assertFalse(list.contains(3));
-		Assert.assertFalse(list.contains(4));
-		Assert.assertFalse(list.contains(5));
-		
-		for(int i=1;i<=500;i++) {
-			list.add(i);
-		}
-		Assert.assertEquals(500, list.size());
-		
-		Assert.assertTrue(list.contains(3));
-		Assert.assertTrue(list.contains(4));
-		Assert.assertTrue(list.contains(5));
-		
-		
-		list.remove((Integer)4);
-		list.remove((Integer)3);
-		list.remove((Integer)2);
-		
-		
-		Assert.assertFalse(list.contains(3));
-		Assert.assertFalse(list.contains(4));
-		Assert.assertFalse(list.contains(5));
-		
-		list.add(3);
-		
-		Assert.assertTrue(list.contains(3));
-		
-	}
-	
-	@Test
-	public void testReaddBig() {
-	      SimpleList<Integer> list=new SimpleList<Integer>();
+   @Test
+   public void testReadd() {
+      SimpleList<Integer> list=new SimpleList<Integer>();
 
-	      Assert.assertFalse(list.contains(3));
-	      Assert.assertFalse(list.contains(4));
-	      Assert.assertFalse(list.contains(5));
+      Assert.assertFalse(list.contains(3));
+      Assert.assertFalse(list.contains(4));
+      Assert.assertFalse(list.contains(5));
 
-	      for(int i=1;i<=500;i++) {
-	         list.add(i);
-	      }
-	      Assert.assertEquals(500, list.size());
+      for(int i=1;i<=500;i++) {
+         list.add(i);
+      }
+      Assert.assertEquals(500, list.size());
 
-	      Assert.assertTrue(list.contains(3));
-	      Assert.assertTrue(list.contains(4));
-	      Assert.assertTrue(list.contains(5));
+      Assert.assertTrue(list.contains(3));
+      Assert.assertTrue(list.contains(4));
+      Assert.assertTrue(list.contains(5));
 
 
-	      list.remove((Integer)4);
-	      list.remove((Integer)3);
-	      list.remove((Integer)2);
+      list.remove((Integer)4);
+      list.remove((Integer)3);
+      list.remove((Integer)2);
 
 
-	      Assert.assertFalse(list.contains(3));
-	      Assert.assertFalse(list.contains(4));
-	      Assert.assertFalse(list.contains(5));
+      Assert.assertFalse(list.contains(3));
+      Assert.assertFalse(list.contains(4));
+      Assert.assertFalse(list.contains(5));
 
-	      list.add(3);
+      list.add(3);
 
-	      Assert.assertTrue(list.contains(3));
+      Assert.assertTrue(list.contains(3));
+   }
 
-	      // try some larger numbers
-	      Assert.assertTrue(list.contains(453));
-	      Assert.assertTrue(list.contains(454));
-	      Assert.assertTrue(list.contains(455));
+   @Test
+   public void testReaddBig() {
+      SimpleList<Integer> list=new SimpleList<Integer>();
+
+      Assert.assertFalse(list.contains(3));
+      Assert.assertFalse(list.contains(4));
+      Assert.assertFalse(list.contains(5));
+
+      for(int i=1;i<=500;i++) {
+         list.add(i);
+      }
+      Assert.assertEquals(500, list.size());
+
+      Assert.assertTrue(list.contains(3));
+      Assert.assertTrue(list.contains(4));
+      Assert.assertTrue(list.contains(5));
 
 
-	      list.remove((Integer)454);
-	      list.remove((Integer)453);
-	      list.remove((Integer)452);
+      list.remove((Integer)4);
+      list.remove((Integer)3);
+      list.remove((Integer)2);
 
 
-	      Assert.assertFalse(list.contains(453));
-	      Assert.assertFalse(list.contains(454));
-	      Assert.assertFalse(list.contains(455));
+      Assert.assertFalse(list.contains(3));
+      Assert.assertFalse(list.contains(4));
+      Assert.assertFalse(list.contains(5));
 
-	      list.add(453);
+      list.add(3);
 
-	      Assert.assertTrue(list.contains(453));
+      Assert.assertTrue(list.contains(3));
 
-	   } 
+      // try some larger numbers
+      Assert.assertTrue(list.contains(453));
+      Assert.assertTrue(list.contains(454));
+      Assert.assertTrue(list.contains(455));
+
+
+      list.remove((Integer)454);
+      list.remove((Integer)453);
+      list.remove((Integer)452);
+
+
+      Assert.assertFalse(list.contains(453));
+      Assert.assertFalse(list.contains(454));
+      Assert.assertFalse(list.contains(455));
+
+      list.add(453);
+
+      Assert.assertTrue(list.contains(453));
+
+   } 
 }
