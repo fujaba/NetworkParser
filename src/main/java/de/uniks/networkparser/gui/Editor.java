e998bfc00d090d448bc5377d1d0ad001bf871e10
Stefan Lindel <slin@cs.uni-kassel.de>
2015-12-04 10:42:49 +0100

    new COnst

--- src/main/java/de/uniks/networkparser/gui/Editor.java
+++ src/main/java/de/uniks/networkparser/gui/Editor.java
@@ -3,6 +3,7 @@ package de.uniks.networkparser.gui;
 import de.uniks.networkparser.json.JsonObject;
 
 public interface Editor {
+	public static final String URL="de.uniks.networkparser.gui.javafx.window.DiagramEditor";
 	public void open(Object logic, String... args);
 	public boolean generate(JsonObject model);
 	public String getIcon();
