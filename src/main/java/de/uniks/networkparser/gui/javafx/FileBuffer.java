921bc36b659a74314ba4b0d89c476c46db7c8d1a
Stefan Lindel <slin@cs.uni-kassel.de>
2015-12-16 16:55:18 +0100

    fix some Copyright and add some Tasks to gradle

--- src/main/java/de/uniks/networkparser/gui/javafx/FileBuffer.java
+++ src/main/java/de/uniks/networkparser/gui/javafx/FileBuffer.java
@@ -1,11 +1,31 @@
 package de.uniks.networkparser.gui.javafx;
 
+/*
+ NetworkParser
+ Copyright (c) 2011 - 2015, Stefan Lindel
+ All rights reserved.
+
+ Licensed under the EUPL, Version 1.1 or (as soon they
+ will be approved by the European Commission) subsequent
+ versions of the EUPL (the "Licence");
+ You may not use this work except in compliance with the Licence.
+ You may obtain a copy of the Licence at:
+
+ http://ec.europa.eu/idabc/eupl5
+
+ Unless required by applicable law or agreed to in
+ writing, software distributed under the Licence is
+ distributed on an "AS IS" basis,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ express or implied.
+ See the Licence for the specific language governing
+ permissions and limitations under the Licence.
+*/
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
 import de.uniks.networkparser.interfaces.Buffer;
 import de.uniks.networkparser.string.CharList;
 
@@ -106,4 +126,3 @@ public class FileBuffer extends Buffer{
 		return value;
 	}
 }
-
