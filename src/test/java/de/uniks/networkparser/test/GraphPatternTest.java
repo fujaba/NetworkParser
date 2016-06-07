5a8c1481fd7372d13c7becee877a7c47e12e1ff0
Stefan Lindel <Stefan.Lindel@cs.uni-kassel.de>
2016-06-07 09:54:12 +0200

    write more tests

--- src/test/java/de/uniks/networkparser/test/GraphPatternTest.java
+++ src/test/java/de/uniks/networkparser/test/GraphPatternTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import de.uniks.networkparser.IdMap;
 import de.uniks.networkparser.graph.GraphPatternMatch;
+import de.uniks.networkparser.test.model.Student;
 import de.uniks.networkparser.test.model.University;
 import de.uniks.networkparser.test.model.util.StudentCreator;
 import de.uniks.networkparser.test.model.util.UniversityCreator;
@@ -16,17 +17,39 @@ public class GraphPatternTest {
 		mapA.with(new StudentCreator());
 		mapA.with(new UniversityCreator());
 
-//		IdMap mapB = new IdMap();
-//		mapB.with(new StudentCreator());
-//		mapB.with(new UniversityCreator());
-		
 		University uniA = new University().withName("Uni Kassel");
-		
 		University uniB = new University().withName("Uni Kassel");
 		
 		GraphPatternMatch diff = mapA.getDiff(uniA, uniB, false);
 		
 		Assert.assertEquals(0, diff.size());
+	}
 
+	@Test
+	public void testMatchWithWrongName() {
+		IdMap mapA = new IdMap();
+		mapA.with(new StudentCreator());
+		mapA.with(new UniversityCreator());
+
+		University uniA = new University().withName("Uni Kassel");
+		University uniB = new University().withName("Uni Paderborn");
+		
+		GraphPatternMatch diff = mapA.getDiff(uniA, uniB, false);
+		
+		Assert.assertEquals(1, diff.size());
+	}
+	@Test
+	public void testMatchWithTwoStudents() {
+		IdMap mapA = new IdMap();
+		mapA.with(new StudentCreator());
+		mapA.with(new UniversityCreator());
+
+		University uniA = new University().withName("Uni Kassel");
+		University uniB = new University().withName("Uni Paderborn");
+		uniB.withStudents(new Student().withName("Alex"));
+		
+		GraphPatternMatch diff = mapA.getDiff(uniA, uniB, false);
+		
+		Assert.assertEquals(4, diff.size());
 	}
 }
