55dbfe5da7952b1d4215fa3d8d5326e8b16fdfec
Albert Zuendorf <zuendorf@cs.uni-kassel.de>
2016-11-26 18:48:45 +0100

    added FLOAT

--- src/main/java/de/uniks/networkparser/graph/DataType.java
+++ src/main/java/de/uniks/networkparser/graph/DataType.java
@@ -31,6 +31,7 @@ public class DataType {
 	public static final DataType VOID = new DataType("void");
 	public static final DataType INT = new DataType("int");
 	public static final DataType LONG = new DataType("long");
+   public static final DataType FLOAT = new DataType("float");
 	public static final DataType DOUBLE = new DataType("double");
 	public static final DataType STRING = new DataType("String");
 	public static final DataType BOOLEAN = new DataType("boolean");
