432fa3cdb073f27d0c35ce393ff295c80c96c91a
Stefan Lindel <Stefan.Lindel@cs.uni-kassel.de>
2017-02-21 10:07:40 +0100

    Revert "added removeYou to SendableEntityCreator"
    This reverts commit a1bb8738a73d0de8e24b997d88d0d8ef34d27d9b.

--- src/main/java/de/uniks/networkparser/interfaces/SendableEntityCreator.java
+++ src/main/java/de/uniks/networkparser/interfaces/SendableEntityCreator.java
@@ -73,10 +73,4 @@ public interface SendableEntityCreator {
 	* @return the sendable instance
 	*/
    Object getSendableInstance(boolean prototyp);
-   
-   public default void removeObject(Object entity)
-   {
-      throw new UnsupportedOperationException();
-   }
-
 }
