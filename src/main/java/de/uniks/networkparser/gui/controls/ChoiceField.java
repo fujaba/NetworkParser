7b85023e79fc611fd097eb0f336ed6949bfe4eea
alexw <alexander.weidt@cs.uni-kassel.de>
2017-04-04 11:17:14 +0200

    Fixed ChoiceField and removed output from JavaAdapter

--- src/main/java/de/uniks/networkparser/gui/controls/ChoiceField.java
+++ src/main/java/de/uniks/networkparser/gui/controls/ChoiceField.java
@@ -1,18 +1,50 @@
 package de.uniks.networkparser.gui.controls;
 
-public class ChoiceField extends Input<Boolean> {
+public class ChoiceField extends Input<String> {
 	/*
 	 * Constants
 	 */
 	protected static final String CHECKBOX = "checkbox";
+
 	protected static final String RADIO = "radio";
+
+	protected static final String CHECKED = "checked";
+
+	private boolean checked = false;
+
 
 	public ChoiceField(boolean multi) {
 		super();
-		if(multi) {
+		this.addBaseElements(CHECKED);
+		if (multi) {
 			this.type = RADIO;
-		}else {
+		}
+		else {
 			this.type = CHECKBOX;
 		}
+	}
+
+
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+
+
+	@Override
+	public boolean setValue(String key, Object value) {
+		if (CHECKED.equals(key)) {
+			this.checked = Boolean.valueOf("" + value);
+			return true;
+		}
+		return super.setValue(key, value);
+	}
+
+
+	@Override
+	public Object getValue(String key) {
+		if (CHECKED.equals(key)) {
+			return (checked) ? CHECKED : null;
+		}
+		return super.getValue(key);
 	}
 }
