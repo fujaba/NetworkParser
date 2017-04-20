7d08e5865144b5ab4b3d0c95c971422813ed25c0
Stefan Lindel <Stefan.Lindel@cs.uni-kassel.de>
2017-04-20 13:26:57 +0200

    add Test

--- src/test/TestSimpleGenerator.java
+++ src/test/TestSimpleGenerator.java
@@ -1,10 +1,16 @@
 package test;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import de.uniks.networkparser.TextItems;
+import de.uniks.networkparser.graph.Attribute;
 import de.uniks.networkparser.graph.Cardinality;
 import de.uniks.networkparser.graph.Clazz;
+import de.uniks.networkparser.graph.DataType;
 import de.uniks.networkparser.graph.GraphList;
+import de.uniks.template.Template;
+import de.uniks.template.TemplateResultFile;
 import de.uniks.template.generator.ModelGenerator;
 
 public class TestSimpleGenerator {
@@ -18,5 +24,22 @@ public class TestSimpleGenerator {
 		ModelGenerator javaModelFactory = new ModelGenerator();
 		javaModelFactory.generate("src", classModel);
 //		System.out.println(javaModelFactory.create(classModel));
+	}
+	
+	@Test
+	public void testGeneratorTemplate() {
+		Template template = new Template().withTemplate(
+				"{{#ifnot {{member.type}}==INTERFACE}}"+
+								"Hello World" +
+							"{{#endif}}");
+		
+		Clazz person = new Clazz("Person");
+		Attribute name = person.createAttribute("name", DataType.STRING);
+		TemplateResultFile templateFile = new TemplateResultFile(person, true);
+		TextItems textItems = new TextItems();
+		
+		template.generate(templateFile, textItems, name);
+		
+		Assert.assertEquals("Hello World", templateFile.toString());
 	}
 }
