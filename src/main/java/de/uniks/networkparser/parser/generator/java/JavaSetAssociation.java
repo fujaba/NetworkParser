2f96e1cabf2591b0059033eea3e68ffa0d7fe96e
Philipp Copei <philipp.copei@gmx.net>
2017-05-30 13:52:25 +0200

    fix for set based associations

--- src/main/java/de/uniks/networkparser/parser/generator/java/JavaSetAssociation.java
+++ src/main/java/de/uniks/networkparser/parser/generator/java/JavaSetAssociation.java
@@ -12,7 +12,7 @@ public class JavaSetAssociation extends BasicGenerator {
 	public JavaSetAssociation() {
 		createTemplate("Declaration", Template.DECLARATION,
 				"{{#template VALUE}}",
-				"{{#ifnot {{other.isEdge}}}}",
+				"{{#if {{other.isEdge}}==false}}",
 				"{{#import {{file.member.fullName}}}}" +
 				"   public {{file.member.name}}Set get{{other.Name}}()",
 				"   {",
