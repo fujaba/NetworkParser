d036c5411d10b302198bff026bb05f29b9b138b5
Stefan Lindel <Stefan.Lindel@cs.uni-kassel.de>
2017-06-27 15:20:32 +0200

    remove debugoutput

--- src/main/java/de/uniks/networkparser/ext/generic/ReflectionBlackBoxTester.java
+++ src/main/java/de/uniks/networkparser/ext/generic/ReflectionBlackBoxTester.java
@@ -123,7 +123,7 @@ public class ReflectionBlackBoxTester {
 				continue;
 			}
 			
-			output(clazz.getName()+":"+m.getName(), logger, NetworkParserLog.LOGLEVEL_ERROR);
+//			output(clazz.getName()+":"+m.getName(), logger, NetworkParserLog.LOGLEVEL_ERROR);
 
 			Object[] call = null;
 			m.setAccessible(true);
