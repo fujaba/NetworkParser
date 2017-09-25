f76f77bed6e4daedc02421beb9880a03cc00a38a
Philipp Copei <philipp.copei@gmx.net>
2017-09-25 13:01:16 +0200

    added test for parser error in case of classes without class body content

---
+++ src/test/java/de/uniks/networkparser/test/generator/ParserEndErrorTest.java
@@ -0,0 +1,21 @@
+package de.uniks.networkparser.test.generator;
+
+import org.junit.Test;
+
+import de.uniks.networkparser.ext.ClassModel;
+import de.uniks.networkparser.graph.Clazz;
+
+public class ParserEndErrorTest {
+
+	@Test
+	public void testParserEndError() {
+		ClassModel model = new ClassModel("org.sdmlib.simple.model.test");
+		Clazz person = model.createClazz("Person");
+		person.enableInterface();
+		
+		model.getGenerator().testGeneratedCode();
+		
+		model.getGenerator().testGeneratedCode();
+	}
+	
+}
