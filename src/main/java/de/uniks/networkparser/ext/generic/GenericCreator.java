1095412fa7ea5dbaa57d002bc78d9b99c9cc75db
Stefan Lindel <Stefan@uni-kassel.de>
2017-12-23 16:57:32 +0100

    refactoring

--- src/main/java/de/uniks/networkparser/ext/generic/GenericCreator.java
+++ src/main/java/de/uniks/networkparser/ext/generic/GenericCreator.java
@@ -211,6 +211,7 @@ public class GenericCreator implements SendableEntityCreator {
 		return false;
 	}
 	
+	@SuppressWarnings("deprecation")
 	private boolean isAccess(Field field, Object entity) {
 		try {
 			Method method = field.getClass().getMethod("canAccess", Object.class);
