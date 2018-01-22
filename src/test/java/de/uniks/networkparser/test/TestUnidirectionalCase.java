eb22cc0605c248af1f042df7e87f2f632c165654
Philipp Copei <philipp.copei@gmx.net>
2018-01-22 14:14:43 +0100

    added test for case of self referencing unidirectional assoc, causing parent class to only contain one of two associations as children

---
+++ src/test/java/de/uniks/networkparser/test/TestUnidirectionalCase.java
@@ -0,0 +1,25 @@
+package de.uniks.networkparser.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.uniks.networkparser.ext.ClassModel;
+import de.uniks.networkparser.graph.Association;
+import de.uniks.networkparser.graph.Cardinality;
+import de.uniks.networkparser.graph.Clazz;
+
+public class TestUnidirectionalCase {
+
+	@Test
+	public void testSelfReferencingUnidirectional() {
+		ClassModel model = new ClassModel();
+	
+		Clazz testClazz = model.createClazz("TestClazz");
+	
+		Association assoc = testClazz.createUniDirectional(testClazz, "target", Cardinality.ONE);
+		
+		Assert.assertTrue(testClazz.getAssociations().contains(assoc));
+		Assert.assertTrue(testClazz.getAssociations().contains(assoc.getOther()));
+	}
+	
+}
