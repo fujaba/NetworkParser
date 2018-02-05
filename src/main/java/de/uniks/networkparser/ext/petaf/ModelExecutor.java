0873d5c0a9db2c79bd03465e968519f9f9328000
Stefan Lindel <Stefan@uni-kassel.de>
2018-02-05 16:58:51 +0100

    clean up

--- src/main/java/de/uniks/networkparser/ext/petaf/ModelExecutor.java
+++ src/main/java/de/uniks/networkparser/ext/petaf/ModelExecutor.java
@@ -2,7 +2,6 @@ package de.uniks.networkparser.ext.petaf;
 
 
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 import de.uniks.networkparser.Filter;
@@ -12,35 +11,25 @@ import de.uniks.networkparser.ext.generic.ReflectionLoader;
 import de.uniks.networkparser.interfaces.SimpleEventCondition;
 import de.uniks.networkparser.json.JsonObject;
 
-public class ModelExecutor extends SimpleEventCondition implements Runnable {
-	private SimpleEvent event;
+public class ModelExecutor extends SimpleEventCondition {
 	@Override
 	public boolean update(SimpleEvent event) {
-		this.event = event;
-		
-		return true;
-	}
-
-	@Override
-	public void run() {
 		Filter filter = (Filter) event.getNewValue();
 		UpdateListener listener = (UpdateListener) event.getSource();
 		JsonObject change = (JsonObject) event.getEntity();
 		
 		final FutureTask<Object> query = new FutureTask<Object>(new Callable<Object>() {
-		    @Override
-		    public Object call() {
-		    	return listener.execute(change, filter);
-		    }
+			@Override
+			public Object call() {
+				return listener.execute(change, filter);
+			}
 		});
 		
 		ReflectionLoader.call("runLater", ReflectionLoader.PLATFORM, Runnable.class, query);
-		
 		try {
 			event.withModelValue(query.get());
-		} catch (InterruptedException e) {
-		} catch (ExecutionException e) {
+		} catch (Exception e) {
 		}
+		return true;
 	}
-
 }
