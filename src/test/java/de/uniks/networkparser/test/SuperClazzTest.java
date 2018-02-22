ccb87ebff02a849de257fb7a142d2c15518b8e85
Philipp Copei <philipp.copei@gmx.net>
2018-02-22 11:32:37 +0100

    added test for super clazz addition

---
+++ src/test/java/de/uniks/networkparser/test/SuperClazzTest.java
@@ -0,0 +1,85 @@
+package de.uniks.networkparser.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.uniks.networkparser.ext.ClassModel;
+import de.uniks.networkparser.graph.Clazz;
+
+public class SuperClazzTest {
+
+	@Test
+	public void testSuperAdd() {
+		ClassModel model = new ClassModel();
+		
+		Clazz superClazz = model.createClazz("SuperClazz");
+		
+		Clazz interfaceClazz = model.createClazz("InterfaceClazz");
+		interfaceClazz.enableInterface();
+		
+		Clazz regularClazz = model.createClazz("RegularClazz");
+		
+		regularClazz.withSuperClazz(superClazz, interfaceClazz);
+		
+		Assert.assertEquals(1, regularClazz.getSuperClazzes(false).size());
+		Assert.assertEquals(1, regularClazz.getInterfaces(false).size());
+	}
+	
+	@Test
+	public void testSuperAdd2() {
+		ClassModel model = new ClassModel();
+		
+		Clazz superClazz = model.createClazz("SuperClazz");
+		
+		Clazz interfaceClazz = model.createClazz("InterfaceClazz");
+		interfaceClazz.enableInterface();
+		
+		Clazz regularClazz = model.createClazz("RegularClazz");
+		
+		regularClazz.withSuperClazz(superClazz);
+		regularClazz.withSuperClazz(interfaceClazz);
+		
+		Assert.assertEquals(1, regularClazz.getSuperClazzes(false).size());
+		Assert.assertEquals(1, regularClazz.getInterfaces(false).size());
+	}
+	
+	@Test
+	public void testSuperAdd3() {
+		ClassModel model = new ClassModel();
+		
+		Clazz superClazz = model.createClazz("SuperClazz");
+		
+		Clazz interfaceClazz = model.createClazz("InterfaceClazz");
+		interfaceClazz.enableInterface();
+		
+		Clazz regularClazz = model.createClazz("RegularClazz");
+		
+		regularClazz.withSuperClazz(interfaceClazz);
+		regularClazz.withSuperClazz(superClazz);
+		
+		Assert.assertEquals(1, regularClazz.getSuperClazzes(false).size());
+		Assert.assertEquals(1, regularClazz.getInterfaces(false).size());
+	}
+	
+	@Test
+	public void testSuperAdd4() {
+		ClassModel model = new ClassModel();
+		
+		Clazz superClazz = model.createClazz("SuperClazz");
+		
+		Clazz interfaceClazz = model.createClazz("InterfaceClazz");
+		interfaceClazz.enableInterface();
+		
+		Clazz interfaceClazz2 = model.createClazz("InterfaceClazz2");
+		interfaceClazz2.enableInterface();
+		
+		Clazz regularClazz = model.createClazz("RegularClazz");
+		
+		regularClazz.withSuperClazz(superClazz, interfaceClazz);
+		regularClazz.withSuperClazz(interfaceClazz2);
+		
+		Assert.assertEquals(1, regularClazz.getSuperClazzes(false).size());
+		Assert.assertEquals(2, regularClazz.getInterfaces(false).size());
+	}
+	
+}
