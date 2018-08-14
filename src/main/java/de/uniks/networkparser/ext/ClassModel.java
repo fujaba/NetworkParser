ce1373763f4acc3513398b0317f0aa26b38a4e15
Albert Zuendorf <zuendorf@cs.uni-kassel.de>
2018-08-14 19:48:40 +0200

    testing network parser codegen improved class diagram dump

--- src/main/java/de/uniks/networkparser/ext/ClassModel.java
+++ src/main/java/de/uniks/networkparser/ext/ClassModel.java
@@ -33,6 +33,28 @@ import de.uniks.networkparser.graph.GraphModel;
 import de.uniks.networkparser.interfaces.BaseItem;
 import de.uniks.networkparser.xml.HTMLEntity;
 
+/**
+ *
+ * <h3>Storyboard StudyRightWithAssignmentsClassGeneration</h3>
+ * <h4><a name = 'step_1'>Step 1: Build model for class University</a></h4>
+ * <pre><code class="java" data-lang="java">
+ *       ClassModel model = new ClassModel(&quot;org.sdmlib.test.examples.studyrightWithAssignments.model&quot;);
+ *
+ *       Clazz universityClass = model.createClazz(&quot;University&quot;)
+ *               .withAttribute(&quot;name&quot;, DataType.STRING);
+ * </code></pre>
+ * <img src="doc-files/StudyRightWithAssignmentsClassGenerationStep2.png" alt="StudyRightWithAssignmentsClassGenerationStep2.png">
+ * <h4><a name = 'step_2'>Step 2: Add class Student</a></h4>
+ * <pre><code class="java" data-lang="java">
+ *       Clazz studentClass = model.createClazz(&quot;Student&quot;)
+ *               .withAttribute(&quot;name&quot;, DataType.STRING)
+ *               .withAttribute(&quot;id&quot;, DataType.STRING)
+ *               .withAttribute(&quot;assignmentPoints&quot;, DataType.INT)
+ *               .withAttribute(&quot;motivation&quot;, DataType.INT)
+ *               .withAttribute(&quot;credits&quot;, DataType.INT);
+ * </code></pre>
+ * <img src="doc-files/StudyRightWithAssignmentsClassGenerationStep5.png" alt="StudyRightWithAssignmentsClassGenerationStep5.png">
+ */
 public class ClassModel extends GraphModel {
 	private ModelGenerator generator = new ModelGenerator().withDefaultModel(this);
 
@@ -74,10 +96,18 @@ public class ClassModel extends GraphModel {
 		return this.generator.getFeature(feature, clazzes);
 	}
 
-	@Override
-	public boolean dumpHTML(String diagramName) {
+	public String dumpHTMLString()
+	{
 		HTMLEntity html = new HTMLEntity();
 		html.withGraph(this);
+		String htmlText = html.toString();
+
+		return htmlText;
+	}
+
+	@Override
+	public boolean dumpHTML(String diagramName) {
+		String htmlText = dumpHTMLString();
 		if(diagramName == null || diagramName.length() < 1) {
 			diagramName = this.getName();
 		}
@@ -87,7 +117,7 @@ public class ClassModel extends GraphModel {
 		if(diagramName.length() < 1) {
 			return false;
 		}
-		return FileBuffer.writeFile("doc/"+diagramName+".html", html.toString())>=0;
+		return FileBuffer.writeFile("doc/"+diagramName+".html", htmlText)>=0;
 	}
 
 	@Override
