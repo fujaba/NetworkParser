c74a622ddcfc7b3bdfb729ae85f97b631d7b8e89
Stefan Lindel <stefan.lindel@yatta.de>
2019-05-14 13:47:09 +0200

    add missed Classes

---
+++ src/main/java/de/uniks/networkparser/SimpleException.java
@@ -0,0 +1,9 @@
+package de.uniks.networkparser;
+
+public class SimpleException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public SimpleException(String msg) {
+		super(msg);
+	}
+}
